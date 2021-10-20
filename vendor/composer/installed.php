2506af3a3c6ddb839f6c0d60ca632018568e737f
Mimouni Bilal <arbreplantebuisson@gmail.com>
2021-10-20 19:17:17 +0200

    'ok'

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b983d730956ebcb4971ec8df89843025b6f05705',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b983d730956ebcb4971ec8df89843025b6f05705',
+            'dev_requirement' => false,
+        ),
+    ),
+);
